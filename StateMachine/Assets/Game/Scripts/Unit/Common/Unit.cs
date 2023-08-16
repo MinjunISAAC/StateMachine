854602ac89e16c9522a454d2dd1fcaf62fd8b890
Min Jun <qkralswns931026@gmail.com>
2023-08-16 19:47:10 +0900

    [DEV] Unit StateMachine 적용

--- StateMachine/Assets/Game/Scripts/Unit/Common/Unit.cs
+++ StateMachine/Assets/Game/Scripts/Unit/Common/Unit.cs
@@ -6,18 +6,89 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace InGame.ForUnit 
-{ 
+{
     public class Unit : MonoBehaviour
     {
+        // --------------------------------------------------
+        // Unit State Enum
+        // --------------------------------------------------
+        public enum EUnitState
+        {
+            Unknown = 0,
+            Idle_Empty = 1,
+            Walk_Empty = 2,
+            Run_Empty = 3,
+        }
+
         // --------------------------------------------------
         // Components
         // --------------------------------------------------
         [Header("Physic Collection")]
         [SerializeField] private Rigidbody _rigidBody = null;
 
+        [Space(1.5f)][Header("Animate Collection")]
+        [SerializeField] private Animator _anim = null;
+
+        // --------------------------------------------------
+        // Variables
+        // --------------------------------------------------
+        // ----- Private
+        private EUnitState _unitState = EUnitState.Unknown;
+
+        private Coroutine _co_CurrntState = null;
+
         // --------------------------------------------------
         // Properties
         // --------------------------------------------------
         public Rigidbody UnitRigidBody { get => _rigidBody; }
+
+        // --------------------------------------------------
+        // Functions - Nomal
+        // --------------------------------------------------
+        // ----- Public
+        public void OnInit() { }
+
+
+        // ----- Private
+        private void _ChangeToUnitState() 
+        { 
+        
+        }
+
+        // ---- State 
+        private void _State_IdleEmpty()
+        {
+
+        }
+
+        private void _State_WalkEmpty() 
+        { 
+            
+        }
+        
+        private void _State_RunEmpty() 
+        { 
+        
+        }
+
+        // --------------------------------------------------
+        // Functions - Coroutine
+        // --------------------------------------------------
+        private IEnumerator _Co_IdleEmpty() 
+        {
+            while (_unitState == EUnitState.Idle_Empty)
+            {
+                yield return null;
+            }
+        }
+
+        private IEnumerator _Co_WalkEmpty()
+        {
+            while (_unitState == EUnitState.Idle_Empty)
+            {
+                yield return null;
+            }
+        }
+        private IEnumerator _Co_RunEmpty() { yield return null; }
     }
 }
