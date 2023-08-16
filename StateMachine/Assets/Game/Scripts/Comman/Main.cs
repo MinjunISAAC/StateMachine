486008923995c1b2d8fee778082c1cae3dd8614f
Min Jun <qkralswns931026@gmail.com>
2023-08-16 19:45:50 +0900

    [DEV] Start 기점 적용

--- StateMachine/Assets/Game/Scripts/Comman/Main.cs
+++ StateMachine/Assets/Game/Scripts/Comman/Main.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 
 // ----- User Defined
 using InGame.ForUnit.Manage;
+using InGame.ForCam;
 
 namespace InGame.Main 
 { 
@@ -16,9 +17,14 @@ namespace InGame.Main
         // Components
         // --------------------------------------------------
         [SerializeField] private UnitController _unitController = null;
+        [SerializeField] private CamController  _camController  = null;
 
         // --------------------------------------------------
         // Functions - Event
         // --------------------------------------------------
+        private void Start()
+        {
+            _camController.OnInit(_unitController.TargetUnit);
+        }
     }
 }
